52de0dcba2d18454d7913f4a4a4b3ac616a7c872
Pavel Churlyaev <cupofwonder@yandex.ru>
2013-12-13 13:13:07 +0400

    init commit

---
+++ RemoteBeanProvider.java
@@ -0,0 +1,42 @@
+package com.sovzond.idspace.client;
+
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * Класс - удаленный поставщик бинов - умеет доставать бины с сервера по JNDI
+ * 
+ * @author pavel-c
+ * 
+ */
+public class RemoteBeanProvider {
+	public Context getContext() throws NamingException {
+		Context ret = null;
+		Properties props = new Properties();
+		props.put("java.naming.factory.initial",
+				"org.jboss.naming.remote.client.InitialContextFactory");
+		props.put(Context.PROVIDER_URL, "remote://localhost:4447");
+		props.put(Context.SECURITY_PRINCIPAL, "ejb");
+		props.put(Context.SECURITY_CREDENTIALS, "ejb1");
+		props.put("java.naming.factory.url.pkgs",
+				"org.jboss.naming:org.jnp.interfaces");
+		props.put("jboss.naming.client.ejb.context", true);
+		return (Context) new InitialContext(props);
+	}
+
+	public Object getBean(String jndiName) {
+		
+		try {
+			Context ctx = getContext();
+			Object bean;
+			bean = ctx.lookup(jndiName);
+			return bean;
+		} catch (NamingException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
